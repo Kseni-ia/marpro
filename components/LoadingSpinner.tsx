da489fcf941d3afeb878a5dab6ed78ced4501e8c
Kseni-ia <sergeevnakseniia9@gmail.com>
2025-11-03 17:32:45 +0100

    changes V10

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gradient-to-br from-gray-800 to-gray-900">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gradient-to-br from-neutral-900 to-neutral-950">
       <div className="loader">
         <div className="inner one"></div>
         <div className="inner two"></div>
